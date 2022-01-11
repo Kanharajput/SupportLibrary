d589f1e0941414072390190fc9d8142e88016d3d
Kanha Tomar <kanharajput91@gmail.com>
2022-01-11 22:42:47 +0530

    textView color changed randomly

--- app/src/main/java/com/example/hellocompat/MainActivity.java
+++ app/src/main/java/com/example/hellocompat/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.hellocompat;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mHelloTextView;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHelloTextView = findViewById(R.id.textView);
+        // restore saved instance state (the text color)
+        if (savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
     }
 
-    public void changeColor(View view) {
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current text color
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
 
+
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName,
+                           "color", getApplicationContext().getPackageName());    // to get the id of the color name
+        // Here we use ContextCompat to run over app over api level below 23 as
+        // getColor method works above 23 api level.
+        int colorRes = ContextCompat.getColor(this,colorResourceName);    // to get the color using id like we do in findViewById method
+        mHelloTextView.setTextColor(colorRes);
     }
 }
